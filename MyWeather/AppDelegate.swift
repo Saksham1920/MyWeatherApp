138c51b605cdb5aba07ea8d2f318966ca7c8e999
Saksham Madan <saksham@Sakshams-MacBook-Air.local>
2021-02-19 22:49:44 +0530

    Add Project

---
+++ MyWeather/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MyWeather
+//
+//  Created by Saksham Madan on 19/02/21.
+//  Copyright © 2021 Saksham Madan. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyWeather/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyWeather
+//
+//  Created by Saksham Madan on 19/02/21.
+//  Copyright © 2021 Saksham Madan. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
